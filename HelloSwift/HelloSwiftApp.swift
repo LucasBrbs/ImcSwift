76961ecb39066de26834b4837d30797b2abf7991
Lucas Barbosa <lucasbarbosa2807@gmail.com>
2022-04-19 09:32:46 -0300

    Initial Commit

---
+++ HelloSwift/HelloSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftApp.swift
+//  HelloSwift
+//
+//  Created by Lucas Barbosa de Oliveira on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
