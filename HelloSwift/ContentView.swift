8bbbe9de63cd4c97bc699f8979a832f80e9ff689
Lucas Barbosa <lucasbarbosa2807@gmail.com>
2022-04-20 10:56:31 -0300

    learning

--- HelloSwift/ContentView.swift
+++ HelloSwift/ContentView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var Height: String = ""
+    @State private var Weight: String = ""
+    @State private var Imc: Double = 0
+    @State private var resposta: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("IMC").font(.largeTitle)
+            
+            TextField("Height", text: $Height).textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(Color.black)
+            
+            TextField("Weight", text: $Weight).textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(Color.black)
+            Button{
+                let weight = Double(self.Weight)!
+                let height = Double(self.Height)!
+                self.Imc = weight/(height * height)
+                if self.Imc < 18.5 {
+                    self.resposta = "abaixo do peso"
+                }
+                else if self.Imc < 24.9 {
+                    self.resposta = "peso ideal"
+                }
+                else if self.Imc < 29.9 {
+                    self.resposta = "acima do peso"
+                }
+                else{
+                    self.resposta = "obeso"
+                }
+            } label: {
+                Text("Calculate")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+            }
+            
+            Text("IMC = \(Imc)")
+            
+            Text("IMC \(Imc, specifier: "%.1f"),\(resposta)").font(.title).padding()
+        }.padding()
+        
     }
 }
 
